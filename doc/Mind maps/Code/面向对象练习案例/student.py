2b165485c4d4b65916126036cae1dd619c901009
Peggy <1083341411@qq.com>
2021-03-09 23:41:07 +0800

    Delete doc/Mind maps/Code/面向对象练习案例 directory

--- doc/Mind maps/Code/面向对象练习案例/student.py
+++
@@ -1,8 +0,0 @@
-class Student(object):
-    def __init__(self,name,gender,tel):
-        self.name=name
-        self.gender=gender
-        self.tel=tel
-
-    def __str__(self):
-        return f'{self.name},{self.gender},{self.tel}'
